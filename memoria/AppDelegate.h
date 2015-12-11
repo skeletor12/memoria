c18a85b1c9d821445712ff1fb45436b0f5868b2b
luisrodriguez <betowas12345@hotmail.com>
2015-12-11 09:45:01 -0600

    hola

---
+++ memoria/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  memoria
+//
+//  Created by Luis Rodriguez on 11/12/15.
+//  Copyright © 2015 skeletor12. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ memoria/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  memoria
+//
+//  Created by Luis Rodriguez on 11/12/15.
+//  Copyright © 2015 skeletor12. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
